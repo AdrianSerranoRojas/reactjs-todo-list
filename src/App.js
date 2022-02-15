bd3d165fd36337b54a2963b556041e01a0b9d77d
Adrián Serrano <adrianno_3@hotmail.com>
2022-02-15 16:25:06 +0100

    layout V1

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
 import React from "react";
 
+import "./App.scss";
+
 function App() {
   return (
     <main className="container mt-5">
       <section className="row">
-        <div className="col col-12">
-          <h1>Hola mundo</h1>
+        <div className="col col-10">
+          <h1>T O D O</h1>
+        </div>
+        <div>Dark Mode</div>
+      </section>
+      <section className="createInput">
+        <form>
+          <input type="text" name="new-task" />
+        </form>
+      </section>
+      <section className="todoList__container">
+        <div className="todoList__item">
+          <input type="checkBox" />
+          <div className="task">Learn HTML</div>
+          <button type="button">Close</button>
+        </div>
+        <div className="todoList__item">
+          <input type="checkBox" />
+          <div className="task">Learn CSS</div>
+          <button type="button">Close</button>
+        </div>
+        <div className="todoList__item">
+          <input type="checkBox" />
+          <div className="task">Learn SASS</div>
+          <button type="button">Close</button>
         </div>
       </section>
     </main>
